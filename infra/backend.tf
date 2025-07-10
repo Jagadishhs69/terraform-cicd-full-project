bd452e29c35252708be8ea2f52d76db2f92ec9ac
Jagadishhs69 <hsjagadish42@gmail.com>
2025-07-10 11:58:47 +0530

    bucket updatre

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-cicd-339933"
-    key = "terraform-state-cicd-339933/qa/terraform.tfstate"
+    bucket = "terraform-state-cicd-33993333"
+    key = "terraform-state-cicd-33993333/qa/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
   
